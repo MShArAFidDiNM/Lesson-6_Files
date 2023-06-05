e55f918063b8b653941ba60b7ca5c61966ee954f
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-06-05 14:49:57 +0500

    Home_Work

---
+++ Files_HomeWork/Files_HomeWork/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Files_HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
