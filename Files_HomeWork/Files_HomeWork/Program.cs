a8e4c4042b52252d7bc556f93f3b2b926280873c
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-06-05 15:45:21 +0500

    BinObj

--- Files_HomeWork/Files_HomeWork/Program.cs
+++ Files_HomeWork/Files_HomeWork/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string path = "C:\\Tekshir";
+            string[] bin = Directory.GetDirectories(path, "bin*.");
+            string[] obj = Directory.GetDirectories(path, "obj*.");
+            foreach (string dir in bin)
+            {
+                Console.WriteLine(dir);
+            }
+            foreach (string dir in obj)
+            {
+                Console.WriteLine(dir);
+            }
         }
+
     }
 }
